062101db15e505a8d506b4f146ecfd611ae4441c
Kevin Bisner <34324949+KBiz65@users.noreply.github.com>
2021-08-25 21:42:49 -0700

    Prepare Express app for Heroku deployment

--- src/server.js
+++ src/server.js
@@ -1,14 +1,5 @@
-const { PORT = 5000 } = process.env;
+const PORT = process.env.PORT || 5000;
 
 const app = require("./app");
-const knex = require("./db/connection");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-
-knex.migrate
-  .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
-  })
-  .catch(console.error);
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}!`));
