2cd6c2d918abdeecee028c099e9f38d05ad0c0a0
Kevin Bisner <34324949+KBiz65@users.noreply.github.com>
2021-08-25 21:31:36 -0700

    Make down migrations more idempotent
    Previously, migrations were running dropTable. This can cause idempotency issues,
    and as a result, I'm converting to the recommended dropTableIfExists.

--- src/db/migrations/20210823082042_createReviewsTable.js
+++ src/db/migrations/20210823082042_createReviewsTable.js
@@ -12,5 +12,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("reviews");
+  return knex.schema.dropTableIfExists("reviews");
 };
--- src/db/migrations/20210823084341_createMovies_TheatersTable.js
+++ src/db/migrations/20210823084341_createMovies_TheatersTable.js
@@ -10,5 +10,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("movies_theaters");
+  return knex.schema.dropTableIfExists("movies_theaters");
 };
